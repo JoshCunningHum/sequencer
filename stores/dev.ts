22ea951534ad78dd454591f89c60527388c73661
JoshCunningHum <jkdimpas@gmail.com>
2024-06-02 08:43:30 +0800

    Dev mode set to false by default

--- stores/dev.ts
+++ stores/dev.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 
 export const useDevStore = defineStore("Dev", () => {
-    const enabled = ref(true);
+    const enabled = ref(false);
 
     return {
         enabled,
